de41ec44b5e48ca9c9f83b051791d500e6df5af7
Wellington Moraes <wellpunk@gmail.com>
2017-11-12 21:09:23 -0200

    Use termite :)

--- debian_scripts/install_terminator_dracula.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "Install terminator..."
-sudo apt install terminator -y
-sleep 1
-echo "Change config to dracula theme :)"
-mkdir $HOME/.config/terminator
-touch $HOME/.config/terminator/config
-curl "https://gist.githubusercontent.com/mswell/c0f05d418f87a9f4c1733186642ea767/raw/403bbdaf8f6b07c9b13349075ecfa524c1f4c85d/config" > $HOME/.config/terminator/config
---
+++ debian_scripts/install_termite.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "Install termite..."
+curl https://raw.githubusercontent.com/mswell/termite-install/master/termite-install.sh -o termite.sh
+chmod +x termite.sh
+./termite.sh
+sleep 1
+echo "Change config to dracula theme :)"
+mkdir $HOME/.config/termite
+cp ../termite_config $HOME/.config/termite/config
+rm termite.sh
