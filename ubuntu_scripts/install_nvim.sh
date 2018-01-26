1655f530dcaee42778a7877e7af2e997c8085943
Wellington Moraes <wellpunk@gmail.com>
2018-01-26 16:40:10 -0200

    Fix issue

--- ubuntu_scripts/install_nvim.sh
+++ ubuntu_scripts/install_nvim.sh
@@ -3,5 +3,5 @@ sudo pip install neovim
 mkdir $HOME/.config/nvim
 cp ../init.vim ~/.config/nvim/
 cp ../local_init.vim ~/.config/nvim/
-cp ../ local_bundles.vim ~/.config/nvim/
+cp ../local_bundles.vim ~/.config/nvim/
 nvim
