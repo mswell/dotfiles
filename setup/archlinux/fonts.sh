30f7bc63e5537c31cec7f6b1ea8e156f0ff1424f
Wellington Moraes <wellpunk@gmail.com>
2019-07-24 15:14:02 -0300

    Fix fonts issue

--- setup/archlinux/fonts.sh
+++ setup/archlinux/fonts.sh
@@ -3,7 +3,7 @@
 echo "Installing fonts"
 
 yay -S $(pacman -Ss ttf | grep -v ^" " | awk '{print $1}') --noconfirm --needed && fc-cache
-yay -S noto-fonts ttf-ancient-fonts --noconfirm --needed
+yay -S noto-fonts powerline-fonts --noconfirm --needed
 yay -S ttf-droid --noconfirm --noconfirm --needed
 yay -S ttf-inconsolata --noconfirm --needed
 
