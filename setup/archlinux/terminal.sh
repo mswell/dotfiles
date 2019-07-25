44228707fbe003cb56e13b57d6ee9bf52f6a7e26
Wellington Moraes <wellpunk@gmail.com>
2019-07-25 08:09:14 -0300

    Fix tmux issue

--- setup/archlinux/terminal.sh
+++ setup/archlinux/terminal.sh
@@ -8,6 +8,7 @@ echo "Logout and login to effective your changes."
 chsh -s $(which zsh)
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/dracula/zsh.git
 mv zsh/dracula.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/themes}
 rm -fr zsh
