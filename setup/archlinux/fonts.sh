6b46fe155c5e72da8b9da6087db76387ad5fce63
Wellington Moraes <wellpunk@gmail.com>
2019-07-31 10:13:25 -0300

    remove powerline fonts and change to Nerdfonts

--- setup/archlinux/fonts.sh
+++ setup/archlinux/fonts.sh
@@ -3,12 +3,8 @@
 echo "Installing fonts"
 
 yay -S $(pacman -Ss ttf | grep -v ^" " | awk '{print $1}') --noconfirm --needed && fc-cache
-yay -S noto-fonts powerline-fonts --noconfirm --needed
+yay -S noto-fonts nerd-fonts-complete --noconfirm --needed
 yay -S ttf-droid --noconfirm --noconfirm --needed
 yay -S ttf-inconsolata --noconfirm --needed
-
-mkdir -p ~/.local/share/fonts
-cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
-
 
 fc-cache -v
