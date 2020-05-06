49b5965ee00c288d6e7f42159750b09671b02b1d
Wellington Moraes <wellpunk@gmail.com>
2020-05-05 23:41:40 -0300

    Fix missing emoji

--- setup/archlinux/fonts.sh
+++ setup/archlinux/fonts.sh
@@ -3,6 +3,7 @@
 echo "Installing fonts"
 
 yay -S $(pacman -Ss ttf | grep -v ^" " | awk '{print $1}') --noconfirm --needed && fc-cache
+yay -s noto-fonts-emoji --noconfirm --needed
 yay -S noto-fonts --noconfirm --needed
 yay -S ttf-droid --noconfirm --noconfirm --needed
 yay -S ttf-inconsolata --noconfirm --needed
