d7daab688357e2ba00b14603b8f4004214b7943e
Wellington Moraes <wellpunk@gmail.com>
2020-05-10 19:18:37 -0300

    Install new tools

--- hack_tools.sh
+++ hack_tools.sh
@@ -13,3 +13,24 @@ go get -u github.com/tomnomnom/meg
 echo "Install ffuf"
 sleep 2
 go get -u github.com/tomnomnom/meg
+
+echo "Install html-tool"
+sleep 2
+go get -u github.com/tomnomnom/hacks/html-tool
+
+echo "Install gf"
+sleep 2
+go get -u github.com/tomnomnom/gf
+cp -r $GOPATH/src/github.com/tomnomnom/gf/examples ~/.gf
+
+echo "Install unurl"
+sleep 2
+go get -u github.com/tomnomnom/unfurl
+
+echo "Install subfinder"
+sleep 2
+go get -v github.com/projectdiscovery/subfinder/cmd/subfinder
+
+echo "Install nuclei"
+sleep 2
+GO111MODULE=on go get -u -v github.com/projectdiscovery/nuclei/cmd/nuclei
