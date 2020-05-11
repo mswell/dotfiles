f8ca9de806d773061e79c7ef4cee67983c7b4e6a
Wellington Moraes <wellpunk@gmail.com>
2020-05-11 16:09:47 -0300

    remove fedora folder and termite install on ubuntu

--- setup/fedora/base.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "Installing base-dev libs"
-sudo dnf install -y @development-tools git vim gimp tlp tlp-rdw vlc xclip curl wget
-sudo tlp start
--- setup/fedora/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-source $DOTFILES/setup/fedora/base.sh
-source $DOTFILES/setup/fedora/devel.sh
-source $DOTFILES/setup/fedora/apps.sh
-source $DOTFILES/setup/fedora/fonts.sh
-source $DOTFILES/setup/fedora/terminal.sh
-source $DOTFILES/setup/fedora/copy_dots.sh
