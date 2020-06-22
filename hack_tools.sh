f2959e5b11b67ec8e31b483f0cd6c7a278ac5db3
Wellington Moraes <wellpunk@gmail.com>
2020-06-22 16:48:46 -0300

    fix update packets

--- hack_tools.sh
+++ hack_tools.sh
@@ -12,7 +12,7 @@ go get -u github.com/tomnomnom/meg
 
 echo "Install ffuf"
 sleep 2
-go get github.com/ffuf/ffuf
+go get -u github.com/ffuf/ffuf
 
 echo "Install html-tool"
 sleep 2
@@ -38,7 +38,7 @@ GO111MODULE=on go get -u -v github.com/projectdiscovery/nuclei/cmd/nuclei
 echo "Install amass"
 sleep 2
 GO111MODULE=on
-go get -v github.com/OWASP/Amass/v3/...
+go get -u -v github.com/OWASP/Amass/v3/...
 
 echo "Install gron"
 sleep 2
@@ -54,7 +54,7 @@ GO111MODULE=on go get -u -v github.com/projectdiscovery/shuffledns/cmd/shuffledn
 
 echo "Install Hakcheckurl"
 sleep 2
-go get github.com/hakluke/hakcheckurl
+go get -u github.com/hakluke/hakcheckurl
 
 echo "Install subjs"
 sleep 2
@@ -62,7 +62,7 @@ GO111MODULE=on go get -u -v github.com/lc/subjs
 
 echo "Install getJS"
 sleep 2
-go get github.com/003random/getJS
+go get -u github.com/003random/getJS
 
 echo "Install gau"
 sleep 2
@@ -73,7 +73,7 @@ sleep 2
 go get -u github.com/Tanmay-N/CORS-Scanner
 
 echo "Install waybackurls"
-go get github.com/tomnomnom/waybackurl
+go get -u github.com/tomnomnom/waybackurl
 
 echo "Install Gospider"
 go get -u github.com/jaeles-project/gospider
