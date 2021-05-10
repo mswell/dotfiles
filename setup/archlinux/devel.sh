da4764aee813d0f9f8aebab1aa4c3f5d24f4d5b3
Wellington Moraes <wellpunk@gmail.com>
2021-05-10 14:31:00 -0300

    change go version

--- setup/archlinux/devel.sh
+++ setup/archlinux/devel.sh
@@ -3,18 +3,16 @@
 echo "Installing tools for developers"
 
 # python and neovim dependencies
-sudo pacman -Syu --noconfirm python python-setuptools
-sudo easy_install pip
-sudo pip install neovim
+sudo pacman -Syu --noconfirm python python-setuptools neovim
 mkdir $HOME/.config/nvim
 
 # install base packages
 
 yay -S --noconfirm --noedit ctags ncurses curl unzip neovim docker docker-compose tmux zsh htop fzf xsel silver-searcher-git tree exa dconf
 echo "Setting Go dev environment"
-wget https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz
-sudo tar -zxvf go1.14.4.linux-amd64.tar.gz -C /usr/local/
-rm go1.14.4.linux-amd64.tar.gz
+wget https://golang.org/dl/go1.16.4.linux-amd64.tar.gz
+sudo tar -zxvf go1.16.4.linux-amd64.tar.gz -C /usr/local/
+rm go1.16.4.linux-amd64.tar.gz
 echo "Set your env!"
 echo "echo 'export GOROOT=/usr/local/go' >> ~/.zshrc"
 echo "echo 'export GOPATH=\$HOME/go' >> ~/.zshrc"
