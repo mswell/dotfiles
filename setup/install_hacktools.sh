7f577c70749105ff93fbfa0e9f6ce257ce500823
Wellington Moraes <wellpunk@gmail.com>
2021-06-19 11:49:30 -0300

    new method to install hacktools

---
+++ setup/install_hacktools.sh
@@ -0,0 +1,147 @@
+#!/usr/bin/env bash
+
+declare -A gotools
+gotools["fff"]="go get -u github.com/tomnomnom/fff"
+gotools["hakrawler"]="go get github.com/hakluke/hakrawler"
+gotools["tojson"]="go get -u github.com/tomnomnom/hacks/tojson"
+gotools["gowitness"]="go get -u github.com/sensepost/gowitness"
+gotools["rush"]="go get -u github.com/shenwei356/rush/"
+gotools["naabu"]="GO111MODULE=on go get -v github.com/projectdiscovery/naabu/cmd/naabu"
+gotools["hakcheckurl"]="go get -u github.com/hakluke/hakcheckurl"
+gotools["shuffledns"]="GO111MODULE=on go get -v github.com/projectdiscovery/shuffledns/cmd/shuffledns"
+gotools["rescope"]="go get -u github.com/root4loot/rescope"
+gotools["gron"]="go get -u github.com/tomnomnom/gron"
+gotools["html-tool"]="go get -u github.com/tomnomnom/hacks/html-tool"
+gotools["Chaos"]="GO111MODULE=on go get -v github.com/projectdiscovery/chaos-client/cmd/chaos"
+gotools["gf"]="go get -v github.com/tomnomnom/gf"
+gotools["qsreplace"]="go get -v github.com/tomnomnom/qsreplace"
+gotools["Amass"]="GO111MODULE=on go get -v github.com/OWASP/Amass/v3/..."
+gotools["ffuf"]="go get -u github.com/ffuf/ffuf"
+gotools["assetfinder"]="go get -v github.com/tomnomnom/assetfinder"
+gotools["github-subdomains"]="go get -u github.com/gwen001/github-subdomains"
+gotools["cf-check"]="go get -v github.com/dwisiswant0/cf-check"
+gotools["waybackurls"]="go get -v github.com/tomnomnom/hacks/waybackurls"
+gotools["nuclei"]="GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei"
+gotools["anew"]="go get -v github.com/tomnomnom/anew"
+gotools["notify"]="GO111MODULE=on go get -v github.com/projectdiscovery/notify/cmd/notify"
+gotools["mildew"]="go get -u github.com/daehee/mildew/cmd/mildew"
+gotools["dirdar"]="go get -u github.com/m4dm0e/dirdar"
+gotools["unfurl"]="go get -v github.com/tomnomnom/unfurl"
+gotools["httpx"]="GO111MODULE=on go get -v github.com/projectdiscovery/httpx/cmd/httpx"
+gotools["github-endpoints"]="go get -u github.com/gwen001/github-endpoints"
+gotools["dnsx"]="GO111MODULE=on go get -v github.com/projectdiscovery/dnsx/cmd/dnsx"
+gotools["subfinder"]="GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder"
+gotools["gauplus"]="GO111MODULE=on go get -u -v github.com/bp0lr/gauplus"
+gotools["subjs"]="GO111MODULE=on go get -u -v github.com/lc/subjs"
+gotools["Gxss"]="go get -v github.com/KathanP19/Gxss"
+gotools["gospider"]="go get -u github.com/jaeles-project/gospider"
+gotools["crobat"]="go get -v github.com/cgboal/sonarsearch/crobat"
+gotools["crlfuzz"]="GO111MODULE=on go get -v github.com/dwisiswant0/crlfuzz/cmd/crlfuzz"
+gotools["dalfox"]="GO111MODULE=on go get -v github.com/hahwul/dalfox/v2"
+gotools["puredns"]="GO111MODULE=on go get github.com/d3mondev/puredns/v2"
+gotools["resolveDomains"]="go get -v github.com/Josue87/resolveDomains"
+gotools["interactsh-client"]="GO111MODULE=on go get -v github.com/projectdiscovery/interactsh/cmd/interactsh-client"
+gotools["analyticsrelationships"]="go get -v github.com/Josue87/analyticsrelationships"
+gotools["gotator"]="go get -v github.com/Josue87/gotator"
+
+declare -A repos
+repos["degoogle_hunter"]="six2dez/degoogle_hunter"
+repos["pwndb"]="davidtavarez/pwndb"
+repos["dnsvalidator"]="vortexau/dnsvalidator"
+repos["dnsrecon"]="darkoperator/dnsrecon"
+repos["theHarvester"]="laramies/theHarvester"
+repos["brutespray"]="x90skysn3k/brutespray"
+repos["wafw00f"]="EnableSecurity/wafw00f"
+repos["gf"]="tomnomnom/gf"
+repos["Gf-Patterns"]="1ndianl33t/Gf-Patterns"
+repos["github-search"]="gwen001/github-search"
+repos["ctfr"]="UnaPibaGeek/ctfr"
+repos["LinkFinder"]="dark-warlord14/LinkFinder"
+repos["Corsy"]="s0md3v/Corsy"
+repos["CMSeeK"]="Tuhinshubhra/CMSeeK"
+repos["fav-up"]="pielco11/fav-up"
+repos["Interlace"]="codingo/Interlace"
+repos["massdns"]="blechschmidt/massdns"
+repos["OpenRedireX"]="devanshbatham/OpenRedireX"
+repos["GitDorker"]="obheda12/GitDorker"
+repos["testssl"]="drwetter/testssl.sh"
+repos["ip2provider"]="oldrho/ip2provider"
+repos["commix"]="commixproject/commix"
+repos["JSA"]="six2dez/JSA"
+repos["urldedupe"]="ameenmaali/urldedupe"
+repos["cloud_enum"]="initstring/cloud_enum"
+repos["JSScanner"]="0x240x23elu/JSScanner"
+
+
+dir="$HOME/Tools"
+
+mkdir -p ~/.gf
+mkdir -p ~/Tools/
+mkdir -p ~/.config/notify/
+mkdir -p ~/.config/amass/
+mkdir -p ~/.config/nuclei/
+mkdir -p ~/Lists/
+
+printf "${bblue} Running: Installing Golang tools (${#gotools[@]})${reset}\n\n"
+go env -w GO111MODULE=auto
+go_step=0
+for gotool in "${!gotools[@]}"; do
+    go_step=$((go_step + 1))
+    eval ${gotools[$gotool]} $DEBUG_STD
+    exit_status=$?
+    if [ $exit_status -eq 0 ]
+    then
+        printf "${yellow} $gotool installed (${go_step}/${#gotools[@]})${reset}\n"
+    else
+        printf "${red} Unable to install $gotool, try manually (${go_step}/${#gotools[@]})${reset}\n"
+        double_check=true
+    fi
+done
+
+eval wget -nc -O ~/Lists/XSS-OFJAAAH.txt https://raw.githubusercontent.com/danielmiessler/SecLists/master/Fuzzing/XSS/XSS-OFJAAAH.txt
+eval wget -nc -O ~/Lists/params.txt https://raw.githubusercontent.com/s0md3v/Arjun/master/arjun/db/params.txt
+
+eval wget -N -c https://bootstrap.pypa.io/get-pip.py $DEBUG_STD && eval python3 get-pip.py $DEBUG_STD
+eval rm -f get-pip.py $DEBUG_STD
+eval ln -s /usr/local/bin/pip3 /usr/bin/pip3 $DEBUG_STD
+eval pip3 install -I -r requirements.txt $DEBUG_STD
+
+eval wget -nc -O ~/.gf/potential.json https://raw.githubusercontent.com/devanshbatham/ParamSpider/master/gf_profiles/potential.json $DEBUG_STD
+
+
+printf "${bblue}\n Running: Installing repositories (${#repos[@]})${reset}\n\n"
+
+cd "$dir" || { echo "Failed to cd to $dir in ${FUNCNAME[0]} @ line ${LINENO}"; exit 1; }
+
+
+# Standard repos installation
+repos_step=0
+for repo in "${!repos[@]}"; do
+    repos_step=$((repos_step + 1))
+    eval git clone https://github.com/${repos[$repo]} $dir/$repo $DEBUG_STD
+    eval cd $dir/$repo $DEBUG_STD
+    eval git pull $DEBUG_STD
+    exit_status=$?
+    if [ $exit_status -eq 0 ]
+    then
+        printf "${yellow} $repo installed (${repos_step}/${#repos[@]})${reset}\n"
+    else
+        printf "${red} Unable to install $repo, try manually (${repos_step}/${#repos[@]})${reset}\n"
+        double_check=true
+    fi
+    if [ -s "setup.py" ]; then
+        eval $SUDO python3 setup.py install $DEBUG_STD
+    fi
+    if [ "massdns" = "$repo" ]; then
+            eval make $DEBUG_STD && strip -s bin/massdns && eval $SUDO cp bin/massdns /usr/bin/ $DEBUG_ERROR
+    elif [ "gf" = "$repo" ]; then
+            eval cp -r examples ~/.gf $DEBUG_ERROR
+    elif [ "Gf-Patterns" = "$repo" ]; then
+            eval mv *.json ~/.gf $DEBUG_ERROR
+    elif [ "urldedupe" = "$repo" ]; then
+            eval cmake CMakeLists.txt $DEBUG_STD
+            eval make $DEBUG_STD
+            eval $SUDO cp ./urldedupe /usr/bin/ $DEBUG_STD
+    fi
+    cd "$dir" || { echo "Failed to cd to $dir in ${FUNCNAME[0]} @ line ${LINENO}"; exit 1; }
+done
