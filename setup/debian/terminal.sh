9441ef1fc84974137dfd46f9f17fa6adad5baa52
Wellington Moraes <wellpunk@gmail.com>
2021-08-05 10:30:52 -0300

    add alacritty option

--- setup/debian/terminal.sh
+++ setup/debian/terminal.sh
@@ -2,7 +2,7 @@
 
 # zsh
 
-sudo apt install -y zsh git autojump tree ttf-ancient-fonts tmux
+sudo apt install -y zsh git autojump tree ttf-ancient-fonts tmux alacritty
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 echo "Now type \`chsh -s $(which zsh)\` to zsh becomes default."
 echo "Logout and login to effective your changes."
