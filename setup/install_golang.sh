f06d1dc1c1902ec337e949a3c5b2437978652027
Wellington Moraes <wellpunk@gmail.com>
2022-04-12 09:34:16 -0300

    update golang version

--- setup/install_golang.sh
+++ setup/install_golang.sh
@@ -2,7 +2,7 @@
 
 echo "Setting Go dev environment"
 
-version="go1.17.5"
+version="go1.18"
 wget https://dl.google.com/go/${version}.linux-amd64.tar.gz $DEBUG_STD
 sudo tar -C /usr/local -xzf ${version}.linux-amd64.tar.gz $DEBUG_STD
 rm -rf go$LATEST_GO*
