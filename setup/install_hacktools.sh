02a117ac715b937e1e38933f136af1fbfd5091ab
Wellington Moraes <wellpunk@gmail.com>
2022-04-12 16:06:54 -0300

    change instalation method

--- setup/install_hacktools.sh
+++ setup/install_hacktools.sh
@@ -6,58 +6,110 @@ DEBUG_ERROR="2>/dev/null"
 # TERM COLORS
 bblue='\033[1;34m'
 
-declare -A gotools
-gotools["fff"]="go install github.com/tomnomnom/fff@latest"
-gotools["hakrawler"]="go install github.com/hakluke/hakrawler@latest"
-gotools["tojson"]="go install github.com/tomnomnom/hacks/tojson@latest"
-gotools["gowitness"]="go install github.com/sensepost/gowitness@latest"
-gotools["rush"]="go install github.com/shenwei356/rush@latest"
-gotools["naabu"]="go install github.com/projectdiscovery/naabu/cmd/naabu@latest"
-gotools["hakcheckurl"]="go install github.com/hakluke/hakcheckurl@latest"
-gotools["shuffledns"]="go install github.com/projectdiscovery/shuffledns/cmd/shuffledns@latest"
-gotools["rescope"]="go install github.com/root4loot/rescope@latest"
-gotools["gron"]="go install github.com/tomnomnom/gron@latest"
-gotools["html-tool"]="go install github.com/tomnomnom/hacks/html-tool@latest"
-gotools["Chaos"]="go install github.com/projectdiscovery/chaos-client/cmd/chaos@latest"
-gotools["gf"]="go install github.com/tomnomnom/gf@latest"
-gotools["qsreplace"]="go install github.com/tomnomnom/qsreplace@latest"
-gotools["Amass"]="go install github.com/OWASP/Amass/v3/...@latest"
-gotools["ffuf"]="go install github.com/ffuf/ffuf@latest"
-gotools["assetfinder"]="go install github.com/tomnomnom/assetfinder@latest"
-gotools["github-subdomains"]="go install github.com/gwen001/github-subdomains@latest"
-gotools["cf-check"]="go install github.com/dwisiswant0/cf-check@latest"
-gotools["waybackurls"]="go install github.com/tomnomnom/hacks/waybackurls@latest"
-gotools["nuclei"]="go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest"
-gotools["anew"]="go install github.com/tomnomnom/anew@latest"
-gotools["notify"]="go install github.com/projectdiscovery/notify/cmd/notify@latest"
-gotools["mildew"]="go install github.com/daehee/mildew/cmd/mildew@latest"
-gotools["dirdar"]="go install github.com/m4dm0e/dirdar@latest"
-gotools["unfurl"]="go install github.com/tomnomnom/unfurl@latest"
-gotools["httpx"]="go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest"
-gotools["github-endpoints"]="go install github.com/gwen001/github-endpoints@latest"
-gotools["dnsx"]="go install github.com/projectdiscovery/dnsx/cmd/dnsx@latest"
-gotools["subfinder"]="go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest"
-gotools["gauplus"]="go install github.com/bp0lr/gauplus@latest"
-gotools["subjs"]="go install github.com/lc/subjs@latest"
-gotools["Gxss"]="go install github.com/KathanP19/Gxss@latest"
-gotools["gospider"]="go install github.com/jaeles-project/gospider@latest"
-gotools["crobat"]="go install github.com/cgboal/sonarsearch/crobat@latest"
-gotools["crlfuzz"]="go install github.com/dwisiswant0/crlfuzz/cmd/crlfuzz@latest"
-gotools["dalfox"]="go install github.com/hahwul/dalfox/v2@latest"
-gotools["puredns"]="go install github.com/d3mondev/puredns/v2@latest"
-gotools["resolveDomains"]="go install github.com/Josue87/resolveDomains@latest"
-gotools["interactsh-client"]="go install github.com/projectdiscovery/interactsh/cmd/interactsh-client@latest"
-gotools["gotator"]="go install github.com/Josue87/gotator@latest"
-gotools["kxss"]="go install github.com/tomnomnom/hacks/kxss@latest"
-gotools["GetJs"]="go install github.com/003random/getJS@latest"
-gotools["Goop"]="go install github.com/deletescape/goop@latest"
-gotools["Meg"]="go install github.com/tomnomnom/meg@latest"
-gotools["Freq"]="go install github.com/takshal/freq@latest"
-gotools["Sdlookup"]="go install github.com/j3ssie/sdlookup@latest"
-gotools["Sigurlfinder"]="go install -v github.com/signedsecurity/sigurlfind3r/cmd/sigurlfind3r@latest"
-gotools["Chromedp"]="go install github.com/chromedp/chromedp@latest"
-gotools["Airixss"]="go install github.com/ferreiraklet/airixss@latest"
-gotools["Nilo"]="go install github.com/ferreiraklet/nilo@latest"
+printf "${bblue} Running: Installing Golang tools (${#gotools[@]})${reset}\n\n"
+
+echo "Install fff"
+go install github.com/tomnomnom/fff@latest
+echo "Install hakrawler"
+go install github.com/hakluke/hakrawler@latest
+echo "Install tojson"
+go install github.com/tomnomnom/hacks/tojson@latest
+echo "Install gowitness"
+go install github.com/sensepost/gowitness@latest
+echo "Install rush"
+go install github.com/shenwei356/rush@latest
+echo "Install naabu"
+go install github.com/projectdiscovery/naabu/cmd/naabu@latest
+echo "Install hakcheckurl"
+go install github.com/hakluke/hakcheckurl@latest
+echo "Install shuffledns"
+go install github.com/projectdiscovery/shuffledns/cmd/shuffledns@latest
+echo "Install rescope"
+go install github.com/root4loot/rescope@latest
+echo "Install gron"
+go install github.com/tomnomnom/gron@latest
+echo "Install html-tool"
+go install github.com/tomnomnom/hacks/html-tool@latest
+echo "Install Chaos"
+go install github.com/projectdiscovery/chaos-client/cmd/chaos@latest
+echo "Install gf"
+go install github.com/tomnomnom/gf@latest
+echo "Install qsreplace"
+go install github.com/tomnomnom/qsreplace@latest
+echo "Install Amass"
+go install github.com/OWASP/Amass/v3/...@latest
+echo "Install ffuf"
+go install github.com/ffuf/ffuf@latest
+echo "Install assetfinder"
+go install github.com/tomnomnom/assetfinder@latest
+echo "Install github-subdomains"
+go install github.com/gwen001/github-subdomains@latest
+echo "Install cf-check"
+go install github.com/dwisiswant0/cf-check@latest
+echo "Install waybackurls"
+go install github.com/tomnomnom/hacks/waybackurls@latest
+echo "Install nuclei"
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+echo "Install anew"
+go install github.com/tomnomnom/anew@latest
+echo "Install notify"
+go install github.com/projectdiscovery/notify/cmd/notify@latest
+echo "Install mildew"
+go install github.com/daehee/mildew/cmd/mildew@latest
+echo "Install dirdar"
+go install github.com/m4dm0e/dirdar@latest
+echo "Install unfurl"
+go install github.com/tomnomnom/unfurl@latest
+echo "Install httpx"
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+echo "Install github-endpoints"
+go install github.com/gwen001/github-endpoints@latest
+echo "Install dnsx"
+go install github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+echo "Install subfinder"
+go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+echo "Install gauplus"
+go install github.com/bp0lr/gauplus@latest
+echo "Install subjs"
+go install github.com/lc/subjs@latest
+echo "Install Gxss"
+go install github.com/KathanP19/Gxss@latest
+echo "Install gospider"
+go install github.com/jaeles-project/gospider@latest
+echo "Install crobat"
+go install github.com/cgboal/sonarsearch/crobat@latest
+echo "Install crlfuzz"
+go install github.com/dwisiswant0/crlfuzz/cmd/crlfuzz@latest
+echo "Install dalfox"
+go install github.com/hahwul/dalfox/v2@latest
+echo "Install puredns"
+go install github.com/d3mondev/puredns/v2@latest
+echo "Install resolveDomains"
+go install github.com/Josue87/resolveDomains@latest
+echo "Install interactsh-client"
+go install github.com/projectdiscovery/interactsh/cmd/interactsh-client@latest
+echo "Install gotator"
+go install github.com/Josue87/gotator@latest
+echo "Install kxss"
+go install github.com/tomnomnom/hacks/kxss@latest
+echo "Install GetJs"
+go install github.com/003random/getJS@latest
+echo "Install Goop"
+go install github.com/deletescape/goop@latest
+echo "Install Meg"
+go install github.com/tomnomnom/meg@latest
+echo "Install Freq"
+go install github.com/takshal/freq@latest
+echo "Install Sdlookup"
+go install github.com/j3ssie/sdlookup@latest
+echo "Install Sigurlfinder"
+go install -v github.com/signedsecurity/sigurlfind3r/cmd/sigurlfind3r@latest
+echo "Install Chromedp"
+go install github.com/chromedp/chromedp@latest
+echo "Install Airixss"
+go install github.com/ferreiraklet/airixss@latest
+echo "Install Nilo"
+go install github.com/ferreiraklet/nilo@latest
 
 declare -A repos
 repos["gf"]="tomnomnom/gf"
@@ -86,20 +138,6 @@ mkdir -p ~/.config/nuclei/
 mkdir -p ~/Lists/
 
 pip3 install uro
-
-printf "${bblue} Running: Installing Golang tools (${#gotools[@]})${reset}\n\n"
-go env -w GO111MODULE=auto
-go_step=0
-for gotool in "${!gotools[@]}"; do
-    go_step=$((go_step + 1))
-    eval ${gotools[$gotool]} $DEBUG_STD
-    exit_status=$?
-    if [ $exit_status -eq 0 ]; then
-        printf "${yellow} $gotool installed (${go_step}/${#gotools[@]})${reset}\n"
-    else
-        printf "${red} Unable to install $gotool, try manually (${go_step}/${#gotools[@]})${reset}\n"
-    fi
-done
 
 eval wget -nc -O ~/Lists/XSS-OFJAAAH.txt https://raw.githubusercontent.com/danielmiessler/SecLists/master/Fuzzing/XSS/XSS-OFJAAAH.txt
 eval wget -nc -O ~/Lists/params.txt https://raw.githubusercontent.com/s0md3v/Arjun/master/arjun/db/params.txt
