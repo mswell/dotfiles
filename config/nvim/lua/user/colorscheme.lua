b1636248f761e6f19e509640087d6192658e5584
Wellington Moraes <wellpunk@gmail.com>
2022-06-02 14:48:05 -0300

    change vim configs

--- config/nvim/lua/user/colorscheme.lua
+++ config/nvim/lua/user/colorscheme.lua
@@ -1,5 +1,7 @@
 vim.cmd [[
+packadd! dracula_pro
 try
+  let g:dracula_colorterm = 0
   colorscheme dracula_pro
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -58,11 +58,10 @@ return packer.startup(function(use)
   use "goolord/alpha-nvim"
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
   use "folke/which-key.nvim"
-  use 'wakatime/vim-wakatime'
-  use 'andweeb/presence.nvim'
+  use "wakatime/vim-wakatime"
 
   -- Colorschemes
-  -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
+  use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
 
   -- cmp plugins
