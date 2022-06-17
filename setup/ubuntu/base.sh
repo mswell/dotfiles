1e25c4988a10d3b4f78d24f12cf1198ea15ca23f
Wellington Moraes <wellpunk@gmail.com>
2022-06-17 15:49:56 -0300

    fix confirmation with nala install

--- setup/ubuntu/base.sh
+++ setup/ubuntu/base.sh
@@ -3,7 +3,7 @@ echo "Install Nala"
 echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
 wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
 echo "deb-src http://deb.volian.org/volian/ scar main" | sudo tee -a /etc/apt/sources.list.d/volian-archive-scar-unstable.list
-sudo apt update && sudo apt install nala
+sudo apt update && sudo apt install nala -y
 sudo nala fetch
 
 echo "Installing base-dev libs"
