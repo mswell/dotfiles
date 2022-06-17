54f302cbc39bbf22f992afe7ab0ae173fcbe7d99
Wellington Moraes <wellpunk@gmail.com>
2022-06-17 15:09:01 -0300

    fix issue in setup.sh

--- setup/ubuntu/setup.sh
+++ setup/ubuntu/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-source $DOTFILES/setup/ubuntu/devel.sh
 source $DOTFILES/setup/ubuntu/base.sh
+source $DOTFILES/setup/ubuntu/devel.sh
 source $DOTFILES/setup/install_golang.sh
 source $DOTFILES/setup/ubuntu/apps.sh
 source $DOTFILES/setup/ubuntu/fonts.sh
