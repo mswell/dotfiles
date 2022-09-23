685338c5d7ab45a38b1730150f42630c7d1218b6
Wellington Moraes <wellpunk@gmail.com>
2022-09-23 10:58:22 -0300

    add new function

--- config/zsh/functions.zsh
+++ config/zsh/functions.zsh
@@ -13,7 +13,6 @@ green=`tput setaf 2`
 yellow=`tput setaf 3`
 reset=`tput sgr0`
 
-
 #################
 # FUNCOES
 #################
@@ -101,13 +100,8 @@ getalive() {
 #------------------------------
 # Coleta + Mapeamento especifica para RedBull
 ReconRedbull(){
-  # naabu -l clean.subdomains -top-ports 100 -sa -o naabuScan
-  # [ -s "naabuScan" ] && cat naabuScan | anew clean.subdomains
-  #httpx -l clean.subdomains -silent -status-code -tech-detect -title -timeout 60 -threads 100 -o HTTPOK
-  #cat HTTPOK | grep 200 | awk -F " " '{print $1}' | anew 200HTTP
-  #cat HTTPOK | grep -E '40[0-4]' | grep -Ev 404 | awk -F " " '{print $1}' | anew 403HTTP
-  #cat HTTPOK | grep -v 404 | awk '{print $1}' | anew Without404
-  #cat HTTPOK | awk -F " " '{print $1}' | anew ALLHTTP
+  naabuRecon
+  [ -s "naabuScan" ] && cat naabuScan | anew clean.subdomains
   getalive
   dnsrecords
   graphqldetect
@@ -119,14 +113,8 @@ ReconRedbull(){
   nucauto
 }
 
-
 swaggerRecon(){
   subdomainenum
-  #httpx -l clean.subdomains -silent -status-code -tech-detect -title -timeout 60 -threads 100 -o HTTPOK
-  #cat HTTPOK | grep 200 | awk -F " " '{print $1}' | anew 200HTTP
-  #cat HTTPOK | grep -E '40[0-4]' | grep -Ev 404 | awk -F " " '{print $1}' | anew 403HTTP
-  #cat HTTPOK | grep -v 404 | awk '{print $1}' | anew Without404
-  #cat HTTPOK | awk -F " " '{print $1}' | anew ALLHTTP
   getalive
   swaggerUIdetect
 }
@@ -135,14 +123,8 @@ newRecon(){
   subdomainenum
   [ -s "asn" ] && cat asn | metabigor net --asn | anew cidr
   [ -s "cidr" ] && cat cidr | anew clean.subdomains
-  # Scan ports - https://github.com/projectdiscovery/naabu
-  naabu -l clean.subdomains -top-ports 100 -sa -o naabuScan
+  naabuRecon
   [ -s "naabuScan" ] && cat naabuScan | anew clean.subdomains
-  #httpx -l clean.subdomains -silent -status-code -tech-detect -title -timeout 60 -threads 100 -o HTTPOK
-  #cat HTTPOK | grep 200 | awk -F " " '{print $1}' | anew 200HTTP
-  #cat HTTPOK | grep -E '40[0-4]' | grep -Ev 404 | awk -F " " '{print $1}' | anew 403HTTP
-  #cat HTTPOK | grep -v 404 | awk '{print $1}' | anew Without404
-  #cat HTTPOK | awk -F " " '{print $1}' | anew ALLHTTP
   getalive
   dnsrecords
   graphqldetect
@@ -163,6 +145,10 @@ GitScan () {
         [ -s "gitvector" ] && cat gitvector | notify -silent
 }
 
+naabuRecon () {
+  # Scan ports - https://github.com/projectdiscovery/naabu
+  naabu -l clean.subdomains -p 80,443,81,300,591,593,832,981,1010,1311,1099,2082,2095,2096,2480,3000,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5280,5281,5601,5800,6543,7000,7001,7396,7474,8000,8001,8008,8014,8042,8060,8069,8080,8081,8083,8088,8090,8091,8095,8118,8123,8172,8181,8222,8243,8280,8281,8333,8337,8443,8500,8834,8880,8888,8983,9000,9001,9043,9060,9080,9090,9091,9200,9443,9502,9800,9981,10000,10250,11371,12443,15672,16080,17778,18091,18092,20720,32000,55440,55672 -sa -o naabuScan
+}
 
 secrets () {
 
