e47b3928db857cf9d5e55b66ca4c67366e0bfe59
Wellington Moraes <wellpunk@gmail.com>
2023-01-24 19:07:33 -0300

    remove unused fonts

--- setup/archlinux/fonts.sh
+++ setup/archlinux/fonts.sh
@@ -4,8 +4,7 @@ echo "Installing fonts"
 
 paru -S $(pacman -Ss ttf | grep -v ^" " | awk '{print $1}') --noconfirm --needed && fc-cache
 paru -S noto-fonts-emoji --noconfirm --needed
-paru -S noto-fonts otf-font-awesome nerd-fonts-mononoki ttf-ubuntu-font-family --noconfirm --needed
-paru -S nerd-fonts-fantasque-sans-mono --noconfirm --noconfirm --needed
+paru -S noto-fonts otf-font-awesome ttf-ubuntu-font-family --noconfirm --needed
 paru -S ttf-inconsolata --noconfirm --needed
 paru -S ttf-hack ttf-iosevka-nerd --needed --noconfirm
 fc-cache -v
