29af0ac26c7c60f7b1cc20fef0bbee400067d94d
Wellington Moraes <wellpunk@gmail.com>
2023-04-11 20:28:43 -0300

    change picom.conf opacity

--- setup/archlinux/fonts.sh
+++ setup/archlinux/fonts.sh
@@ -7,4 +7,5 @@ paru -S noto-fonts-emoji --noconfirm --needed
 paru -S noto-fonts otf-font-awesome ttf-ubuntu-font-family --noconfirm --needed
 paru -S ttf-inconsolata --noconfirm --needed
 paru -S ttf-hack ttf-iosevka-nerd --needed --noconfirm
+paru -S ttf-fantasque-nerd --needed --noconfirm
 fc-cache -v
