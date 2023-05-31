d95e0345ffcae2e6330708880b360b35c413e8e1
Wellington Moraes <wellpunk@gmail.com>
2023-05-31 11:34:36 -0300

    fix font size adjustment

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -1,9 +1,11 @@
 local wezterm = require("wezterm")
 
 return {
-	window_background_opacity = 0.9,
-	font_size = 18,
+	window_background_opacity = 0.98,
+	font_size = 16,
 	hide_tab_bar_if_only_one_tab = true,
 	color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
-  font = wezterm.font("FantasqueSansMono Nerd Font"),
+	font = wezterm.font("FantasqueSansM Nerd Font"),
+	default_cursor_style = "BlinkingBar",
+	adjust_window_size_when_changing_font_size = false,
 }
