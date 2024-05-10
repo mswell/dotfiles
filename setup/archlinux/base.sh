917727c7379de1f0f96d5508203995357009ba16
Wellington Moraes <wellpunk@gmail.com>
2024-05-10 18:10:45 -0300

    add more features on i3

--- setup/archlinux/base.sh
+++ setup/archlinux/base.sh
@@ -2,11 +2,5 @@
 
 sudo pacman -S --needed base-devel
 
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd -
-rm -rf yay
-
 echo "Installing base-dev libs"
 yay -Syu --noconfirm git vim python-pip
