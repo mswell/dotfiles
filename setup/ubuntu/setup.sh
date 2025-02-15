afc6e8dc7db888877126ba060beec0fdf281fdaf
Wellington Moraes <wellpunk@gmail.com>
2025-02-15 18:13:39 -0300

    remove fonts script

--- setup/ubuntu/setup.sh
+++ setup/ubuntu/setup.sh
@@ -4,6 +4,5 @@ source $DOTFILES/setup/ubuntu/base.sh
 source $DOTFILES/setup/ubuntu/devel.sh
 source $DOTFILES/setup/install_golang.sh
 source $DOTFILES/setup/ubuntu/apps.sh
-source $DOTFILES/setup/ubuntu/fonts.sh
 source $DOTFILES/setup/ubuntu/terminal.sh
 source $DOTFILES/setup/copy_dots.sh
