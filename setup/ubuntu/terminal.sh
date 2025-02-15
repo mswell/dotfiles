c20227e6a61a2cc67e3fb1d7843c2aa89b4f958f
Wellington Moraes <wellpunk@gmail.com>
2025-02-15 18:36:34 -0300

    add font to starship

--- setup/ubuntu/terminal.sh
+++ setup/ubuntu/terminal.sh
@@ -16,6 +16,12 @@ chsh -s $(which zsh)
 # Prints instructions for the user
 echo "${yellow}[+] Restart the system to apply the changes.${reset}"
 
+wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip \
+&& cd ~/.local/share/fonts \
+&& unzip JetBrainsMono.zip \
+&& rm JetBrainsMono.zip \
+&& fc-cache -fv
+
 # Installs Starship
 echo "${yellow}[+] Installing Starship${reset}"
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
