d2b05c7a7d08f6882fcc9377ebda8be007f05413
Wellington Moraes <wellpunk@gmail.com>
2025-08-25 15:46:48 -0300

    refactor(install): improve readability of font installation

--- setup/ArchHypr/fonts.sh
+++ setup/ArchHypr/fonts.sh
@@ -1,21 +1,42 @@
 #!/bin/sh
 set -e
 
-# Function to install packages with pacman
-install_pacman() {
-  sudo pacman -S --noconfirm "$@"
-}
+# --- Font Lists ---
 
-# Function to install packages with yay
-install_yay() {
-  yay -S --noconfirm --needed "$@"
-}
+official_fonts=(
+    ttf-cascadia-code-nerd
+    ttf-cascadia-mono-nerd
+    ttf-fira-code
+    ttf-fira-mono
+    ttf-fira-sans
+    ttf-firacode-nerd
+    ttf-iosevka-nerd
+    ttf-iosevkaterm-nerd
+    ttf-jetbrains-mono-nerd
+    ttf-jetbrains-mono
+    ttf-nerd-fonts-symbols
+    ttf-nerd-fonts-symbols-mono
+)
 
-# Install fonts
-echo "Installing fonts"
-install_pacman ttf-cascadia-code-nerd ttf-cascadia-mono-nerd ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-firacode-nerd ttf-iosevka-nerd ttf-iosevkaterm-nerd ttf-jetbrains-mono-nerd ttf-jetbrains-mono ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono
-install_yay noto-fonts-emoji
-install_yay noto-fonts otf-font-awesome ttf-ubuntu-font-family
-install_yay ttf-inconsolata otf-geist-mono
-install_yay ttf-hack ttf-fantasque-nerd
+aur_fonts=(
+    noto-fonts-emoji
+    noto-fonts
+    otf-font-awesome
+    ttf-ubuntu-font-family
+    ttf-inconsolata
+    otf-geist-mono
+    ttf-hack
+    ttf-fantasque-nerd
+)
+
+# --- Installation ---
+
+echo "Installing fonts from official repositories..."
+sudo pacman -S --noconfirm --needed "${official_fonts[@]}"
+
+echo "Installing fonts from AUR..."
+yay -S --noconfirm --needed "${aur_fonts[@]}"
+
+# Update font cache
+echo "Updating font cache..."
 fc-cache -v
