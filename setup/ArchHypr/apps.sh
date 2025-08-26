3e03c96d3d93fe6e373fe6721e66dc37b5287c5d
Wellington Moraes <wellpunk@gmail.com>
2025-08-26 11:28:39 -0300

    fix instalation problems and set new config to ghostty

--- setup/ArchHypr/apps.sh
+++ setup/ArchHypr/apps.sh
@@ -1,151 +1,76 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 export DOTFILES="$PWD"
 
-# --- Package Lists ---
-
-# Official repositories (pacman)
-official_packages=(
-    hyprland
-    xdg-desktop-portal-hyprland
-    dunst
-    waybar
-    zoxide
-    tofi
-    hyprpicker
-    hyprlock
-    hypridle
-    hyprpaper
-    swappy
-    grimblast
-    wlogout
-    qt5-wayland
-    qt6-wayland
-    pavucontrol
-    vlc
-    wget
-    unclutter
-    curl
-    bash-completion
-    ctags
-    ncurses
-    zsh
-    xclip
-    autojump
-    meld
-    thunar
-    thunar-volman
-    thunar-archive-plugin
-    tumbler
-    gvfs
-    xarchiver
-    ffmpegthumbnailer
-    poppler-glib
-    unrar zip p7zip ntfs-3g
-    python
-    python-setuptools
-    neovim
-    ripgrep
-    fzf
-    docker
-    docker-compose
-    tmux
-    the_silver_searcher
-    tree
-    exa
-    nwg-look
-    qt5ct
-    qt6ct
-    kvantum
-    network-manager-applet
-    kitty
-    pipewire
-    wireplumber
-    pamixer
-    brightnessctl
-    sddm
-    tar
-    lsb-release
-    vim
-    python-pip
-)
-
-# AUR (yay)
-aur_packages=(
-    appimagelauncher-bin
-    bat
-    lsd
-    wezterm
-    cliphist
-    librewolf-bin
-    obsidian
-    git-delta
-    flameshot-git
-    google-chrome
-    openfortivpn
-    ghostty
-    hyprswitch
-    hyprpolkitagent
-    kvantum-theme-catppuccin-git
-    jome
-    waypaper
-    brave-bin
-)
-
-# --- Installation ---
-
-echo "Installing official packages..."
-sudo pacman -S --noconfirm --needed "${official_packages[@]}"
-
-echo "Installing AUR packages..."
-yay -S --noconfirm --needed "${aur_packages[@]}"
-
-
-# --- Configuration ---
-
-# Function to copy config files if they don't exist
-copy_config() {
-    local config_path="$1"
-    local dest_path="$HOME/.config/$config_path"
-    if [ ! -d "$dest_path" ]; then
-        echo "Copying $config_path configs..."
-        mkdir -p "$(dirname "$dest_path")"
-        cp -r "$DOTFILES/config/$config_path" "$(dirname "$dest_path")/"
-    else
-        echo "$config_path configs already exist. Skipping."
-    fi
+# Function to install packages with pacman
+install_pacman() {
+    sudo pacman -S --noconfirm "$@"
 }
 
-copy_config "hypr"
-copy_config "kitty"
-copy_config "dunst"
-copy_config "waybar"
-copy_config "tofi"
-copy_config "wlogout"
+# Function to install packages with yay
+install_yay() {
+    yay -S --noconfirm --needed "$@"
+}
+
+# Hyprland installation and configuration
+echo "Install Hyprland"
+install_pacman hyprland
+mkdir -p $HOME/.config/hypr/ && cp -r $DOTFILES/config/hypr $HOME/.config/
+mkdir -p $HOME/.config/kitty/ && cp -r $DOTFILES/config/kitty $HOME/.config/
+
+install_pacman xdg-desktop-portal-hyprland dunst
+cp -r $DOTFILES/config/dunst $HOME/.config/
+
+echo "Installing useful Apps"
+
+# Software from 'normal' repositories
+install_yay tmux qt5-wayland qt6-wayland
+install_yay bat lsd wezterm cliphist librewolf-bin obsidian pavucontrol
+install_yay git-delta vlc flameshot-git wget unclutter curl bash-completion
+install_yay ctags lazygit ncurses zsh xclip autojump google-chrome
+install_yay meld discord openfortivpn fzf ghostty
+install_yay thunar thunar-volman thunar-archive-plugin tumbler
+install_yay gvfs xarchiver ffmpegthumbnailer poppler-glib gvfs-mtp gvfs-nfs gvfs-smb unrar zip p7zip ntfs-3g
+
+# Installation of zippers and unzippers
+install_yay unace unrar zip unzip sharutils uudeview arj cabextract
+
+# Install Python and Neovim dependencies
+install_yay python python-setuptools neovim
 
 # Create Neovim configuration directory, if it doesn't exist
-if [ ! -d "$HOME/.config/nvim" ]; then
-    mkdir -p "$HOME/.config/nvim"
-fi
+mkdir -p "$HOME/.config/nvim"
+
+# Instala pacotes base para desenvolvimento
+install_yay ripgrep fzf curl unzip neovim docker docker-compose tmux the_silver_searcher tree exa nwg-look
 
 # Bat config
-if [ ! -f "$(bat --config-dir)/themes/tokyonight_night.tmTheme" ]; then
-    mkdir -p "$(bat --config-dir)/themes"
-    wget -P "$(bat --config-dir)/themes" https://raw.githubusercontent.com/folke/tokyonight.nvim/main/extras/sublime/tokyonight_night.tmTheme
-    bat cache --build
-fi
+mkdir -p "$(bat --config-dir)/themes"
+wget -P "$(bat --config-dir)/themes" https://raw.githubusercontent.com/folke/tokyonight.nvim/main/extras/sublime/tokyonight_night.tmTheme
+bat cache --build
 
-# Backgrounds
-if [ ! -d "$HOME/.config/backgrounds" ]; then
-    mkdir -p "$HOME/.config/backgrounds/"
-    cp -r "$DOTFILES/config/backgrounds" "$HOME/.config/"
-fi
+# Instalação de Waybar e configuração
+install_pacman waybar zoxide
+cp -r $DOTFILES/config/waybar $HOME/.config/
 
-# Themes
+# Instalação de Tofi e configuração
+install_yay tofi
+cp -r $DOTFILES/config/tofi $HOME/.config/
+
+# Instalação de Hyprpicker, Hyprlock e Hypridle e configuração
+install_yay hyprpicker hyprlock hypridle hyprpicker hyprswitch hyprpaper hyprpolkitagent
+
+mkdir -p $HOME/.config/backgrounds/ && cp -r $DOTFILES/config/backgrounds $HOME/.config/
+
+# Instalação de Wlogout e Grimblast e configuração
+install_yay wlogout grimblast
+cp -r $DOTFILES/config/wlogout $HOME/.config/
+
+# Instalação de temas
 echo "Installing themes"
+install_yay kvantum-theme-catppuccin-git
+install_pacman nwg-look qt5ct qt6ct kvantum waypaper
+
 sudo tar -xvf $DOTFILES/config/themes/Catppuccin-Mocha.tar.xz -C /usr/share/themes/
 sudo tar -xvf $DOTFILES/config/icons/Tela-circle-dracula.tar.xz -C /usr/share/icons/
-
-echo "Enabling SDDM..."
-sudo systemctl enable sddm.service
