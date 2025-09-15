a723a16f891c57a56382afeb199afaf05d6348f4
Wellington Moraes <wellpunk@gmail.com>
2025-09-15 19:13:46 -0300

    refactor(install): otimiza script de apps do ArchHypr
    - Consolida pacotes em listas únicas para pacman e yay.
    - Remove instalações duplicadas.
    - Substitui xclip por wl-clipboard para melhor suporte a Wayland.

--- setup/ArchHypr/apps.sh
+++ setup/ArchHypr/apps.sh
@@ -5,7 +5,7 @@ export DOTFILES="$PWD"
 
 # Function to install packages with pacman
 install_pacman() {
-    sudo pacman -S --noconfirm "$@"
+    sudo pacman -S --noconfirm --needed "$@"
 }
 
 # Function to install packages with yay
@@ -13,64 +13,126 @@ install_yay() {
     yay -S --noconfirm --needed "$@"
 }
 
-# Hyprland installation and configuration
-echo "Install Hyprland"
-install_pacman hyprland
-mkdir -p $HOME/.config/hypr/ && cp -r $DOTFILES/config/hypr $HOME/.config/
-mkdir -p $HOME/.config/kitty/ && cp -r $DOTFILES/config/kitty $HOME/.config/
+# --- Package Lists ---
 
-install_pacman xdg-desktop-portal-hyprland dunst
-cp -r $DOTFILES/config/dunst $HOME/.config/
+# Official repository packages (pacman)
+pacman_packages="
+    hyprland
+    xdg-desktop-portal-hyprland
+    dunst
+    waybar
+    zoxide
+    nwg-look
+    qt5ct
+    qt6ct
+    kvantum
+    waypaper
+"
 
-echo "Installing useful Apps"
+# AUR and other packages (yay)
+yay_packages="
+    tmux
+    qt5-wayland
+    qt6-wayland
+    bat
+    lsd
+    wezterm
+    cliphist
+    wl-clipboard
+    librewolf-bin
+    obsidian
+    pavucontrol
+    git-delta
+    vlc
+    flameshot-git
+    wget
+    unclutter
+    curl
+    bash-completion
+    ctags
+    lazygit
+    ncurses
+    zsh
+    autojump
+    google-chrome
+    meld
+    discord
+    openfortivpn
+    fzf
+    ghostty
+    thunar
+    thunar-volman
+    thunar-archive-plugin
+    tumbler
+    gvfs
+    xarchiver
+    ffmpegthumbnailer
+    poppler-glib
+    gvfs-mtp
+    gvfs-nfs
+    gvfs-smb
+    unrar
+    zip
+    p7zip
+    ntfs-3g
+    unace
+    unzip
+    sharutils
+    uudeview
+    arj
+    cabextract
+    python
+    python-setuptools
+    neovim
+    ripgrep
+    docker
+    docker-compose
+    the_silver_searcher
+    tree
+    exa
+    tofi
+    hyprpicker
+    hyprlock
+    hypridle
+    hyprswitch
+    hyprpaper
+    hyprpolkitagent
+    wlogout
+    grimblast
+    kvantum-theme-catppuccin-git
+"
 
-# Software from 'normal' repositories
-install_yay tmux qt5-wayland qt6-wayland
-install_yay bat lsd wezterm cliphist librewolf-bin obsidian pavucontrol
-install_yay git-delta vlc flameshot-git wget unclutter curl bash-completion
-install_yay ctags lazygit ncurses zsh xclip autojump google-chrome
-install_yay meld discord openfortivpn fzf ghostty
-install_yay thunar thunar-volman thunar-archive-plugin tumbler
-install_yay gvfs xarchiver ffmpegthumbnailer poppler-glib gvfs-mtp gvfs-nfs gvfs-smb unrar zip p7zip ntfs-3g
+# --- Installation ---
 
-# Installation of zippers and unzippers
-install_yay unace unrar zip unzip sharutils uudeview arj cabextract
+echo "Installing packages from official repositories..."
+install_pacman $pacman_packages
 
-# Install Python and Neovim dependencies
-install_yay python python-setuptools neovim
+echo "Installing packages from AUR and other sources..."
+install_yay $yay_packages
+
+# --- Configuration ---
+
+echo "Copying configuration files..."
+mkdir -p "$HOME/.config/hypr" && cp -r "$DOTFILES/config/hypr" "$HOME/.config/"
+mkdir -p "$HOME/.config/kitty" && cp -r "$DOTFILES/config/kitty" "$HOME/.config/"
+cp -r "$DOTFILES/config/dunst" "$HOME/.config/"
+cp -r "$DOTFILES/config/waybar" "$HOME/.config/"
+cp -r "$DOTFILES/config/tofi" "$HOME/.config/"
+mkdir -p "$HOME/.config/backgrounds" && cp -r "$DOTFILES/config/backgrounds" "$HOME/.config/"
+cp -r "$DOTFILES/config/wlogout" "$HOME/.config/"
 
 # Create Neovim configuration directory, if it doesn't exist
 mkdir -p "$HOME/.config/nvim"
 
-# Install base packages for development
-install_yay ripgrep fzf curl unzip neovim docker docker-compose tmux the_silver_searcher tree exa nwg-look
-
 # Bat config
+echo "Configuring bat..."
 mkdir -p "$(bat --config-dir)/themes"
 wget -P "$(bat --config-dir)/themes" https://raw.githubusercontent.com/folke/tokyonight.nvim/main/extras/sublime/tokyonight_night.tmTheme
 bat cache --build
 
-# Waybar installation and configuration
-install_pacman waybar zoxide
-cp -r $DOTFILES/config/waybar $HOME/.config/
-
-# Tofi installation and configuration
-install_yay tofi
-cp -r $DOTFILES/config/tofi $HOME/.config/
-
-# Hyprpicker, Hyprlock and Hypridle installation and configuration
-install_yay hyprpicker hyprlock hypridle hyprpicker hyprswitch hyprpaper hyprpolkitagent
-
-mkdir -p $HOME/.config/backgrounds/ && cp -r $DOTFILES/config/backgrounds $HOME/.config/
-
-# Wlogout and Grimblast installation and configuration
-install_yay wlogout grimblast
-cp -r $DOTFILES/config/wlogout $HOME/.config/
-
 # Installing themes
-echo "Installing themes"
-install_yay kvantum-theme-catppuccin-git
-install_pacman nwg-look qt5ct qt6ct kvantum waypaper
+echo "Extracting themes..."
+sudo tar -xvf "$DOTFILES/config/themes/Catppuccin-Mocha.tar.xz" -C /usr/share/themes/
+sudo tar -xvf "$DOTFILES/config/icons/Tela-circle-dracula.tar.xz" -C /usr/share/icons/
 
-sudo tar -xvf $DOTFILES/config/themes/Catppuccin-Mocha.tar.xz -C /usr/share/themes/
-sudo tar -xvf $DOTFILES/config/icons/Tela-circle-dracula.tar.xz -C /usr/share/icons/
+echo "Apps installation and configuration completed."
