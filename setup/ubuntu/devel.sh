666c74fec51db253801133a0fb523dcb3c6107d9
Wellington Moraes <wellpunk@gmail.com>
2025-10-02 21:24:58 -0300

    feat: add support to debian systems and fix some bugs

--- setup/ubuntu/devel.sh
+++ setup/ubuntu/devel.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-URL_NEOVIM="https://raw.githubusercontent.com/LunarVim/LunarVim/rolling/utils/installer/install-neovim-from-release"
 URL_RUST="https://sh.rustup.rs"
 
 red=$(tput setaf 1)
@@ -11,10 +10,10 @@ reset=$(tput sgr0)
 echo "${yellow}[+] Install tools for developers${reset}"
 
 echo "${yellow}[+] Install python & neovim dependencies${reset}"
-sudo -H pip3 install --upgrade pynvim virtualenvwrapper
+sudo -H pip3 install --upgrade pynvim virtualenvwrapper --break-system-packages
 
 [ ! -d "$HOME/.config/nvim" ] && mkdir -p $HOME/.config/nvim
-wget -q -O - --no-check-certificate $URL_NEOVIM | bash
+sudo apt install -y neovim
 
 sudo apt update
 sudo apt install -y vim-nox tmux git exuberant-ctags zsh tree htop ncurses-term silversearcher-ag curl npm
